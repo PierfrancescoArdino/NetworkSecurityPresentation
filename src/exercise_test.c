665bd613f7faead845b460cb1fa68eb6a91c34d0
Pierfrancesco Ardino <pierfrancesco.ardino@studenti.unitn.it>
2017-05-02 14:01:38 +0200

    added test exercise

---
+++ src/exercise_test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void test(int a , int b , int c ) {
+    char buffer1[4] = { 'A' , 'B' , 'C' , 'D' } ;
+    int t = a;
+    int f = b;
+    int x = c;
+}
+
+int main (int argc, char **argv) {
+    test(1, 2, 3);
+    return 0;
+}
